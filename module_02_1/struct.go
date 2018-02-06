92e9fff45bf86abe264bcef9b6bf697d3e7f640a
ashiina <ahmad.n.shiina@gmail.com>
2018-02-06 02:55:33 +0000

    fixed module2

--- module_02_1/struct.go
+++ module_02_1/struct.go
@@ -7,14 +7,44 @@ type Human struct {
 	age  int
 }
 
+func (h Human) WalkTo(destination string) {
+	fmt.Println("I'm walking to", destination)
+}
+
+func (h Human) Introduce() {
+	fmt.Println("My name is", h.name, "and I am", h.age, "years old.")
+}
+
 type Student struct {
 	Human
 	speciality string
 }
 
+func (s *Student) SetSpeciality(speciality string) {
+	s.speciality = speciality
+}
+
+func (s Student) Introduce() {
+	fmt.Println("My name is", s.name, "and I am", s.age, "years old. I study", s.speciality)
+}
+
+type Walker interface {
+	WalkTo(destination string)
+}
+
 func main() {
-	P := Student{Human: Human{name: "Steve", age: 20}, speciality: "Math"}
-	fmt.Println(P.name)
-	fmt.Println(P.age)
-	fmt.Println(P.speciality)
+	bob := Human{name: "Bob", age: 60}
+	bob.Introduce()
+
+	steve := Student{Human: Human{name: "Steve", age: 30}, speciality: "Design"}
+	steve.Introduce()
+	steve.SetSpeciality("Business")
+	steve.Introduce()
+
+	WalkToNewYork(bob)
+	WalkToNewYork(steve)
+}
+
+func WalkToNewYork(w Walker) {
+	w.WalkTo("New York City")
 }
